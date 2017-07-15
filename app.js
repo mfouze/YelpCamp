aff7cd773dc444472bad28c7cd0f9cfcee00d0b8
mfouze <m_fouze93@yahoo.fr>
2017-07-15 17:03:39 +0200

    Initialisation du projet et first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get('/', function(req, res){
+    res.send("landing");
+
+});
+//ceci est un commentaire unique 
+
+app.listen(8085, function(){
+    console.log("server has started");
+});
